6ad215e389d9d8015bf0886b10a6208be96bb7e7
Daniel Kim <heesu07@gmail.com>
2021-09-06 09:19:13 +1200

    add .env

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,21 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCTfMu-PVTq6qZ8E3U8b8LsNczTIwaTwXM",
-  authDomain: "todo-e8c5f.firebaseapp.com",
-  projectId: "todo-e8c5f",
-  storageBucket: "todo-e8c5f.appspot.com",
-  messagingSenderId: "823710539640",
-  appId: "1:823710539640:web:2c7ca3f2a8cd4a85d7b28d"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_MESSAGING_APP_ID
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCTfMu-PVTq6qZ8E3U8b8LsNczTIwaTwXM",
+//   authDomain: "todo-e8c5f.firebaseapp.com",
+//   projectId: "todo-e8c5f",
+//   storageBucket: "todo-e8c5f.appspot.com",
+//   messagingSenderId: "823710539640",
+//   appId: "1:823710539640:web:2c7ca3f2a8cd4a85d7b28d"
+// };
 
 // v8
 firebase.initializeApp(firebaseConfig);
